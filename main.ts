ad0177c041692dddea9f81413f66c6af6ccd9f33
sifi8953 <simon.fikse.8953@student.uu.se>
2024-09-12 09:26:22 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,13 @@ input.onGesture(Gesture.Shake, function () {
 	
 })
 function Connect () {
-	
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 }
 radio.setGroup(101)
 basic.forever(function () {
