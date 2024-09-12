4b28cf0966f7c79813c1ea9f83bae29092585cfc
RawCalcium <101475374+RawCalcium@users.noreply.github.com>
2024-09-12 09:55:27 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    if (currentlyShooting) {
+        led.unplot(shootingTargetX, shootingTargetY)
+        shootingTargetX += 1
+        if (shootingTargetX > 4) {
+            shootingTargetX = 0
+        }
+        led.plot(shootingTargetX, shootingTargetY)
+    }
+})
 function Shoot (playerToShoot: number) {
     shootingTargetX = 0
     shootingTargetY = 0
+    currentlyShooting = true
     led.plot(shootingTargetX, shootingTargetY)
 }
+input.onButtonPressed(Button.B, function () {
+    if (currentlyShooting) {
+        led.unplot(shootingTargetX, shootingTargetY)
+        shootingTargetY += 1
+        if (shootingTargetY > 4) {
+            shootingTargetY = 0
+        }
+        led.plot(shootingTargetX, shootingTargetY)
+    }
+})
 let shootingTargetY = 0
 let shootingTargetX = 0
+let currentlyShooting = false
 radio.setGroup(101)
+currentlyShooting = false
 Shoot(1)
 basic.forever(function () {
 	
