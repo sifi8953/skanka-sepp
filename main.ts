099ef29e9170ba53e664c92926bac85bc92cd6aa
sifi8953 <simon.fikse.8953@student.uu.se>
2024-09-12 10:08:24 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,11 +9,15 @@ function connect () {
         . . . . .
         . . . . .
         `)
-    for (let index = 0; index < 50; index++) {
-        if (connection_state == "searching") {
+    t0 = control.millis()
+    while (connection_state == "searching") {
+        if (control.millis() - t0 < 5000) {
             radio.sendString("join request")
             basic.pause(100)
         } else {
+            this_id = 0
+            player_count = 1
+            connection_state = "connected"
             break;
         }
     }
@@ -29,11 +33,19 @@ radio.onReceivedValue(function (name, value) {
     if (connection_state == "searching") {
         if (name == "join anwer") {
             this_id = value
+            player_count = value + 1
+            connection_state = "connected"
+            radio.sendValue("joining as", this_id)
+        }
+    } else if (connection_state == "connected") {
+        if (name == "joinging as") {
+            player_count = value + 1
             radio.sendValue("joining as", this_id)
         }
     }
 })
 let this_id = 0
+let t0 = 0
 let connection_state = ""
 let player_count = 0
 connect()
