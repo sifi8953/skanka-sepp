899414a560524ee37e01be012251e756efe6c53f
sifi8953 <simon.fikse.8953@student.uu.se>
2024-09-12 09:52:17 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,36 @@
 function connect () {
     radio.setGroup(101)
+    player_count = 0
+    connection_state = "searching"
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    for (let index = 0; index < 50; index++) {
+    	
+    }
+    while (connection_state == "searching") {
+        if (connection_state == "searching") {
+            radio.sendString("join request")
+            basic.pause(100)
+        }
+    }
 }
-input.onGesture(Gesture.Shake, function () {
-	
+radio.onReceivedString(function (receivedString) {
+    if (connection_state == "connected") {
+        if (receivedString == "join request") {
+            radio.sendValue("join answer", player_count)
+        }
+    }
 })
-radio.setGroup(101)
-basic.forever(function () {
-	
+radio.onReceivedValue(function (name, value) {
+    if (connection_state == "searching") {
+    	
+    }
 })
+let connection_state = ""
+let player_count = 0
+connect()
