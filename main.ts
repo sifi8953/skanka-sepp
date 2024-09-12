95bd66b00577c69baf2f04b8d74c4e334e761d51
RawCalcium <101475374+RawCalcium@users.noreply.github.com>
2024-09-12 09:31:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
-input.onGesture(Gesture.Shake, function () {
-	
-})
+function Shoot (playerToShoot: number) {
+    shootingTargetX = 0
+    shootingTargetY = 0
+    led.plot(shootingTargetX, shootingTargetY)
+}
+let shootingTargetY = 0
+let shootingTargetX = 0
 radio.setGroup(101)
+Shoot(1)
 basic.forever(function () {
 	
 })
