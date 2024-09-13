ac0e3a6b72fa6ddd50d328e5773ca95747902f85
sifi8953 <simon.fikse.8953@student.uu.se>
2024-09-13 12:22:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,12 +7,25 @@ function extract_to (msg: string) {
 function pack_msg (to: number, id: number, _type: number, msg: string) {
     return "" + long_to_str(to) + long_to_str(id) + String.fromCharCode(_type) + msg
 }
+function req_handler (_from: number, id: number, _type: number, msg: string) {
+    if (_type == 203) {
+        return "abcd"
+    } else {
+        return ""
+    }
+}
 function extract_type (msg: string) {
     return msg.charCodeAt(4)
 }
 function extract_msg (msg: string) {
     return msg.substr(5, msg.length - 5)
 }
+control.onEvent(1024, 203, function () {
+    basic.showString(received)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (send_req(0, 203, "hej")))
+})
 function send_fin (to: number, id: number) {
     radio.sendString("" + (pack_msg(to, id, 1, "")))
 }
@@ -27,22 +40,12 @@ function send_res (to: number, id: number, msg: string, event: number) {
     1
     )
 }
-input.onButtonPressed(Button.A, function () {
-    basic.showString("" + (send_req(0, 203, "hej")))
-})
 function extract_id (msg: string) {
     return str_to_long(msg.substr(2, 2))
 }
-function send_req (to: number, _type: number, msg: string) {
-    message = pack_msg(to, control.millis(), _type, msg)
-    event_channel = 2
-    control.raiseEvent(
-    1024,
-    1
-    )
-    control.waitForEvent(1024, 2)
-    return response
-}
+control.onEvent(1024, 102, function () {
+    basic.showString(received)
+})
 radio.onReceivedString(function (receivedString) {
     if (extract_to(receivedString) == 0 || extract_to(receivedString) == control.deviceSerialNumber()) {
         if (extract_type(receivedString) == 1) {
@@ -69,11 +72,21 @@ radio.onReceivedString(function (receivedString) {
             if (handled_ids.indexOf(extract_id(message)) == -1) {
                 handled_ids.push(extract_id(message))
                 received = extract_msg(receivedString)
-                send_res(radio.receivedPacket(RadioPacketProperty.SerialNumber), extract_id(receivedString), "abc", extract_type(receivedString))
+                send_res(radio.receivedPacket(RadioPacketProperty.SerialNumber), extract_id(receivedString), req_handler(radio.receivedPacket(RadioPacketProperty.SerialNumber), extract_id(receivedString), extract_type(receivedString), extract_msg(receivedString)), extract_type(receivedString))
             }
         }
     }
 })
+function send_req (to: number, _type: number, msg: string) {
+    message = pack_msg(to, control.millis(), _type, msg)
+    event_channel = 2
+    control.raiseEvent(
+    1024,
+    1
+    )
+    control.waitForEvent(1024, 2)
+    return response
+}
 input.onButtonPressed(Button.B, function () {
     send_msg(0, 102, "123")
 })
@@ -86,11 +99,11 @@ function send_msg (to: number, _type: number, msg: string) {
     )
     control.waitForEvent(1024, 2)
 }
-let received = ""
-let awaiting = false
 let response = ""
+let awaiting = false
 let event_channel = 0
 let message = ""
+let received = ""
 let handled_ids: number[] = []
 handled_ids = []
 radio.setGroup(101)
